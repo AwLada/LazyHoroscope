9c2178c52f57d90235ef92fc569ffb3cba1bc042
awlada <awlada@mail.csuchico.edu>
2019-09-23 16:16:26 -0700

    Nickname + fortune V.1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors: Adrian Lada
 #include<iostream>
 #include<string>
 
@@ -7,10 +7,18 @@ using namespace std;
 int main()
 {
   //declare variables
-
+string first;
+string last;
+int lucky;
   //get user input
+cout<<"What is your first name?"<<endl;
+cin>>first;
+cout<<"What is your last name?"<<endl;
+cin>>last;
 
+lucky = first.length();
   //tell fortune
-
+cout<<"Welcome, "<<first.at(0)<<"."<<last.at(0)<<"., here is your fortune..."<<endl;
+cout<<"your lucky number is "<<lucky<<endl;
   return 0;
 }
