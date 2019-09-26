151439ebf56e9cb725ac431e15aa9e1c09be2aab
awlada <awlada@mail.csuchico.edu>
2019-09-26 12:50:27 -0700

    Added Vowel Variables to determine fortune

--- main.cpp
+++ main.cpp
@@ -20,5 +20,28 @@ lucky = first.length();
   //tell fortune
 cout<<"Welcome, "<<first.at(0)<<"."<<last.at(0)<<"., here is your fortune..."<<endl;
 cout<<"your lucky number is "<<lucky<<endl;
-  return 0;
+//--------------------------------------------------------------------------------------
+if (toupper (first.at(0)) == 'A' || toupper (first.at(0)) == 'E' || toupper (first.at(0)) == 'I' || toupper (first.at(0)) == 'O' || (first.at(0)) == 'U' )
+
+// (first.at(0) == 'A' || first.at(0) == 'E' || first.at(0) == 'I' || first.at(0) == 'O' || first.at(0) == 'U' )
+ {         
+         
+        cout<<"you are destined to be famous!"<<endl;
+
+}
+else
+{
+        cout<<"you should keep a low profile."<<endl;
+}
+
+if (toupper (last.at(last.length()-1)) == 'E' || toupper (last.at(last.length()-1)) == 'A' || toupper (last.at(last.length()-1)) == 'U' || toupper (last.at(last.length()-1)) == 'I' || toupper (last.at(last.length()-1)) == 'O')
+
+/* (toupper (last.at(last.length())) == 'A' || toupper (last.at(strlen(last)) == 'E' || toupper (last.at(strlen(last)) == 'U' || toupper (last.at(strlen(last)) == 'O' || toupper (last.at(strlen(last)) == 'I')
+*/
+{
+        cout<<"you have already met you true love."<<endl;
+}
+
+cout<<"have a good day!"<<endl;
+return 0;
 }
